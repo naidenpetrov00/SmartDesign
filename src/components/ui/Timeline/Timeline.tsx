be8585a932aae68d17c2af65e66d225c51cce994
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2025-07-12 12:36:52 +0300

    Step Icon Import

--- src/components/ui/Timeline/Timeline.tsx
+++ src/components/ui/Timeline/Timeline.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 
 import StepConnector, {
   stepConnectorClasses,
-} from '@mui/material/StepConnector';
-import { Box } from '@mui/material';
-import Step from '@mui/material/Step';
-import Stepper from '@mui/material/Stepper';
-import { styled } from '@mui/material/styles';
-import StepLabel from '@mui/material/StepLabel';
-import { StepIconProps } from '@mui/material/StepIcon';
-import PriceChangeIcon from '@mui/icons-material/PriceChange';
-import ConstructionIcon from '@mui/icons-material/Construction';
-import TroubleshootIcon from '@mui/icons-material/Troubleshoot';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+} from "@mui/material/StepConnector";
+import { Box } from "@mui/material";
+import Step from "@mui/material/Step";
+import Stepper from "@mui/material/Stepper";
+import { styled } from "@mui/material/styles";
+import StepLabel from "@mui/material/StepLabel";
+import StepIconProps from "@mui/material/StepIcon";
+import PriceChangeIcon from "@mui/icons-material/PriceChange";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import TroubleshootIcon from "@mui/icons-material/Troubleshoot";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
 
-import { useSteps } from '../../../hooks/useSteps';
+import { useSteps } from "../../../hooks/useSteps";
 
 const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
@@ -24,39 +24,39 @@ const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
       backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
     },
   },
   [`&.${stepConnectorClasses.completed}`]: {
     [`& .${stepConnectorClasses.line}`]: {
       backgroundImage:
-        'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
     },
   },
   [`& .${stepConnectorClasses.line}`]: {
     height: 3,
     border: 0,
-    backgroundColor: '#eaeaf0',
+    backgroundColor: "#eaeaf0",
     borderRadius: 1,
-    ...theme.applyStyles('dark', {
+    ...theme.applyStyles("dark", {
       backgroundColor: theme.palette.grey[800],
     }),
   },
 }));
 
-const ColorlibStepIconRoot = styled('div')<{
+const ColorlibStepIconRoot = styled("div")<{
   ownerState: { completed?: boolean; active?: boolean };
 }>(({ theme }) => ({
-  backgroundColor: '#ccc',
+  backgroundColor: "#ccc",
   zIndex: 1,
-  color: '#fff',
+  color: "#fff",
   width: 50,
   height: 50,
-  display: 'flex',
-  borderRadius: '50%',
-  justifyContent: 'center',
-  alignItems: 'center',
-  ...theme.applyStyles('dark', {
+  display: "flex",
+  borderRadius: "50%",
+  justifyContent: "center",
+  alignItems: "center",
+  ...theme.applyStyles("dark", {
     backgroundColor: theme.palette.grey[700],
   }),
   variants: [
@@ -64,15 +64,15 @@ const ColorlibStepIconRoot = styled('div')<{
       props: ({ ownerState }) => ownerState.active,
       style: {
         backgroundImage:
-          'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-        boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
+          "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+        boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
       },
     },
     {
       props: ({ ownerState }) => ownerState.completed,
       style: {
         backgroundImage:
-          'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
+          "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
       },
     },
   ],
@@ -118,8 +118,8 @@ export default function Timeline() {
               StepIconComponent={ColorlibStepIcon}
               sx={{
                 borderRadius: 2,
-                borderColor: 'black',
-                '& .MuiStepLabel-label': { color: 'black !important' },
+                borderColor: "black",
+                "& .MuiStepLabel-label": { color: "black !important" },
               }}
             >
               {label}
