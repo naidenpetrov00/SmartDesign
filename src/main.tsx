24e9edb842e52999255d6aed156ab90b817f9e40
naidenpetrov00 <naiden.petrov.31.12.00@gmail.com>
2025-11-08 15:36:49 +0200

    Pause App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import "./utils/i18n/i18n";
+
+// import App from "./app/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./utils/i18n/i18n";
-import App from "./app/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
   </StrictMode>
 );
